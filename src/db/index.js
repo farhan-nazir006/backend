b8597303da614df4447b1522adaefd541eee2dd5
farhan-nazir006 <farhannazir006@gmail.com>
2025-10-18 15:41:49 +0500

    add user registration and using locally mongodb due to error in Atlas

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,11 @@ import { DB_NAME } from "../constants.js";
 const connectDb = async () => {
 
   try {
+
+   // const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
     
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
+   //Local
+    const connectionInstance = await mongoose.connect(`mongodb://localhost:27017/videotube`)
 
     console.log("MongoDb Connected successfuly !! " , connectionInstance.connection.host);
     
